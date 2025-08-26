520aa2e3e18e00765b39ceb236401b6c9556885a
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-08-26 17:26:44 -0300

    feat(construction): construction page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,69 @@
+import Image from 'next/image';
+import { AlertTriangle } from 'lucide-react';
+import { Icon } from '@/components/atoms/icons/Icon';
+import { socialMediaLinks } from '@/data/links';
+import { cn } from '@/lib/utils';
+import DarkOverlay from '@/components/atoms/backgrounds/DarkOverlay';
+import SocialIconButton from '@/components/atoms/buttons/SocialIconButton';
 
 export default function Home() {
   return (
-      <main className="w-dvw h-dvh bg-gradient-to-br from-dark to-zcom">
+    <main className="relative w-dvw h-dvh overflow-hidden text-white flex flex-col xl:flex-row items-center xl:items-start">
+      <div
+        aria-hidden
+        className={cn(
+          'absolute pointer-events-none z-10 bg-construction_bg bg-no-repeat bg-center bg-contain',
+          'bottom-[-200px] left-[-100px] w-[120%] h-[420px]',
+          'md:bottom-[-300px] md:left-[-200px] md:h-[600px]',
+          'xl:bottom-[-500px] xl:left-[-650px] xl:h-[900px]',
+        )}
+      />
+      <div className="absolute inset-0 bg-gradient-to-b xl:bg-gradient-to-br from-dark to-zcom" />
+      <DarkOverlay />
+      <section className="relative w-full h-[40dvh] xl:h-[70dvh] z-20 flex items-center justify-center xl:justify-start xl:order-2">
+        <Image
+          src="/images/construction/under-construction-sign.png"
+          alt='Placa "sob construção" da empresa ZCOM'
+          width={1200}
+          height={800}
+          sizes="(max-width: 768px) 60vw, (max-width: 1280px) 40vw, 33vw"
+          className="object-contain h-full absolute top-0 left-16 md:static md:w-auto"
+          priority={true}
+          quality={100}
+        />
+      </section>
+      <section className="w-full h-[50dvh] xl:h-dvh z-20 flex flex-col items-center justify-center gap-6 px-6 md:px-12 xl:px-32 2xl:px-64">
+        <div className="relative w-full h-fit flex items-center justify-center xl:justify-start">
+          <Icon icon={AlertTriangle} className="w-16 h-16 xl:w-32 xl:h-32 text-zcom" aria-hidden />
+        </div>
 
-      </main>
+        <div className="w-full max-w-3xl text-center xl:text-left">
+          <h1 className="text-2xl md:text-4xl lg:text-3xl 2xl:text-4xl font-semibold tracking-tight italic">
+            Site em <span className="text-zcom">construção</span>!
+          </h1>
+
+          <div className="mt-4 text-sm md:text-base text-white">
+            <p>Nosso site está em construção, mas estamos prontos para começar!</p>
+            <p className="mt-2">A Zcom está preparando algo incrível para vocês! </p>
+            <p className="mt-10">
+              Clique nos ícones abaixo para acessar nossas redes sociais ou entrar em contato para
+              saber mais.
+            </p>
+          </div>
+
+          <div className="mt-6 flex items-center justify-center xl:justify-start gap-4">
+            {socialMediaLinks.map((item) => (
+              <SocialIconButton
+                key={item.title ?? item.link}
+                name={item.title}
+                target={item.target}
+                icon={item.icon}
+                link={item.link}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
