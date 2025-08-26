149171c32e8521730699c5d82b80ca0c0f3b4597
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-08-26 17:25:24 -0300

    chore: lint and package config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,29 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgo: false,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,24 @@
-import type { Config } from "tailwindcss";
-import animate from "tailwindcss-animate";
+import type { Config } from 'tailwindcss';
+import animate from 'tailwindcss-animate';
 
 const config: Config = {
-    content: [
-        "./app/**/*.{ts,tsx}",
-        "./components/**/*.{ts,tsx}",
-        "./src/**/*.{ts,tsx}",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["var(--font-neometric)", "system-ui", "sans-serif"],
-            },
-            colors: {
-                dark: '#282829',
-                light: '#FEFEFE',
-                zcom: '#0E46B9'
-            },
-        },
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        construction_bg: 'url(/images/construction/background-lines.png)',
+      },
+      fontFamily: {
+        sans: ['var(--font-neometric)', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        dark: '#282829',
+        light: '#FEFEFE',
+        zcom: '#0E46B9',
+      },
     },
-    plugins: [animate],
+  },
+  plugins: [animate],
 };
 
 export default config;
