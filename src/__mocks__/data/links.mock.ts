6d845f681afaa7a35d70c4e1826c67c03d0ce9bd
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-08-27 23:45:30 -0300

    test: mocks

---
+++ src/__mocks__/data/links.mock.ts
@@ -0,0 +1,4 @@
+export const mockSocialMediaLinks = [
+  { title: 'Instagram', link: 'https://instagram.com/zcom', icon: 'InstagramIcon' },
+  { title: 'LinkedIn', link: 'https://linkedin.com/company/zcom', icon: 'LinkedInIcon' },
+];
