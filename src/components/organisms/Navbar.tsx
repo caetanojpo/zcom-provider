24c83cdaa8b84ceca5f98c978b269869dba3ff41
Matheus Fontana <mbfontana@outlook.com>
2025-09-01 22:02:16 -0300

    moving navBar and navBarMenu

---
+++ src/components/organisms/Navbar.tsx
@@ -0,0 +1,79 @@
+import Image from 'next/image';
+import { IMAGE_SRC } from '@/data/imageSrc.data';
+import { NAVBAR_COPYWRITING } from '@/data/copywriting/navbar.data';
+import NavbarMenu from '@/components/molecules/NavbarMenu';
+import { Typography } from '@/components/atoms/text/Typography';
+import SocialIconButton from '@/components/atoms/buttons/SocialIconButton';
+import WhatsAppIcon from '../../../public/icons/whatsapp.svg';
+import { socialMediaLinks } from '@/data/links.data';
+import TextButton from '@/components/atoms/buttons/TextButton';
+import Link from 'next/link';
+
+export default function NavBar() {
+  return (
+    <div
+      className="
+        mx-auto mt-5 w-[95%]
+        rounded-full
+        px-8 md:py-3
+        bg-gradient-to-r from-zcom-dark to-zcom
+        shadow-[inset_0_1px_0_rgba(255,255,255,.06),0_6px_20px_rgba(0,0,0,.35)]
+        border border-white/10
+      "
+    >
+      <div className="flex items-center gap-6">
+        <div className="flex flex-1 items-center gap-20">
+          <Link href="/" className="w-[100px]">
+            <Image
+              src={IMAGE_SRC.navBar.logo}
+              alt={NAVBAR_COPYWRITING.logoAlt}
+              width={100}
+              height={100}
+              priority
+              quality={100}
+            />
+          </Link>
+          <NavbarMenu />
+        </div>
+
+        <div className="flex flex-1 items-center justify-end gap-4">
+          <TextButton
+            href={NAVBAR_COPYWRITING.invoice.link}
+            variant="body-xs"
+            className="text-white/90 hover:text-white transition-colors"
+          >
+            {NAVBAR_COPYWRITING.invoice.text}
+          </TextButton>
+
+          <div className="flex items-center">
+            <SocialIconButton
+              name="whatsapp"
+              target="_blank"
+              icon={WhatsAppIcon}
+              link="https://wa.me/5518996660018"
+              variant="white"
+              size="xs"
+            />
+            <Typography variant="body-xs" className="text-white">
+              (18) 99785-6960
+            </Typography>
+          </div>
+
+          <div className="flex items-center">
+            {socialMediaLinks.map((item, idx) => (
+              <SocialIconButton
+                key={idx}
+                name={item.title}
+                target={item.target}
+                icon={item.icon}
+                link={item.link}
+                variant="white"
+                size="sm"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
