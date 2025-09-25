b84ee38a621c754e4b59d0d457c02233ceb26fc4
Matheus Fontana <mbfontana@outlook.com>
2025-09-25 20:51:27 -0300

    feat: section internet 5g

--- src/app/em-dev-dos-crias/page.tsx
+++ src/app/em-dev-dos-crias/page.tsx
@@ -1,12 +1,14 @@
 import PlansPage from '@/components/pages/PlansPage';
 import HeroPage from '@/components/pages/HeroPage';
 import FiberOpticPage from '@/components/pages/FiberOpticPage';
+import Internet5GPage from '@/components/pages/Internet5GPage';
 
 export default function HomeDev() {
   return (
     <>
       <HeroPage />
       <FiberOpticPage />
+      <Internet5GPage />
       <PlansPage />
     </>
   );
